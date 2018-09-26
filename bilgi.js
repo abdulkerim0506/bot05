12426e105ed4da5e2a4ee9c51313aa3df216dd6d
abdulkerim0506 <43606744+abdulkerim0506@users.noreply.github.com>
2018-09-26 15:19:29 +0300

    Add files via upload

---
+++ bilgi.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const ayarlar = require('../ayarlar.json');
+
+exports.run = (client, message) => {
+  if (message.channel.type !== 'dm') {
+    const ozelmesajkontrol = new Discord.RichEmbed()
+    .setColor(0x00AE86)
+    .setTimestamp()
+    .setAuthor(message.author.username, message.author.avatarURL)
+    .setDescription('Özel Mesajlarına Bilgi Mesajımı Attım! :postbox: ');
+    message.channel.sendEmbed(ozelmesajkontrol) }
+	const pingozel = new Discord.RichEmbed()
+    .setColor(0x00AE86)
+    .setAuthor(message.author.username, message.author.avatarURL)
+    .setDescription('**yap�mc�m OYUN KANAL(0.200K)#9437**');
+    return message.author.sendEmbed(pingozel)
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['bot bilgi', 'botbilgi', 'bb', 'botb', 'bbot', 'hakkında', 'bot hakkında', 'bothakkında'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'bilgi',
+  description: 'Bot ile ilgili bilgi verir.',
+  usage: 'bilgi'
+};
